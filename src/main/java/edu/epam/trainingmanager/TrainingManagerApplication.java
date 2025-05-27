748f9f213e3aaa6f32f3467372623836888d1089
polikarmike <polikarpov.mike@yandex.ru>
2025-05-28 01:52:06 +0400

    Initial commit

---
+++ src/main/java/edu/epam/trainingmanager/TrainingManagerApplication.java
@@ -0,0 +1,13 @@
+package edu.epam.trainingmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/epam/trainingmanager/TrainingManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.epam.trainingmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
